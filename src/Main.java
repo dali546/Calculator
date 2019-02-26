cd629db057bfa20d773f9a68e1516fd214d13298
dali546 <35352237+dali546@users.noreply.github.com>
2019-02-26 13:37:14 +0000

    Calculator 4 operators

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a parameter ('+','-','*','/'");
+        String input = scanner.next();
+
+        //Random random = new Random();
+        System.out.println("Enter a number");
+        int randomNumber = scanner.nextInt();
+        System.out.println("Enter another number");
+        int randomNumberTwo = scanner.nextInt();
+        System.out.println(randomNumber);
+        System.out.println(randomNumberTwo);
+        switch (input) {
+            case "+":
+                System.out.println(randomNumber + randomNumberTwo);
+                break;
+            case "-":
+                System.out.println(randomNumber - randomNumberTwo);
+                break;
+            case "*":
+                System.out.println(randomNumber * randomNumberTwo);
+                break;
+            case "/":
+                System.out.println(randomNumber / randomNumberTwo);
+                break;
+            default:
+                System.out.println("Error. Invalid Operator.");
+                break;
+        }
     }
 }
